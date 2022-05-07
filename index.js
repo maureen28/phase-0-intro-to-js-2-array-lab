fd381e373ce7a71520ffc3f01df8e5dd43efe206
maureen <maureen.murimi@moringaschool.com>
2022-05-07 15:01:58 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+let cats =  ["Milo", "Otis", "Garfield"]
+
+let destructivelyAppendCat = name => cats.push(name)
+       
+let destructivelyPrependCat = name => cats.unshift(name)
+  
+let destructivelyRemoveLastCat = () => cats.pop()
+
+let destructivelyRemoveFirstCat = () => cats.shift()
+
+let appendCat = name => [...cats, name]
+
+let prependCat = name => [name, ...cats]
+
+let removeLastCat = () => cats.slice(0, cats.length -1)
+
+let removeFirstCat = () => cats.slice(1)
+
